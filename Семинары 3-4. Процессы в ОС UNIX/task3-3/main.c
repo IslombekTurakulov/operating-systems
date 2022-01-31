f582d88bd2a14197f3fbf43b78e7fa940fab0f8b
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-01-31 14:07:20 +0300

    Lab 3 done

---
+++ Семинары 3-4. Процессы в ОС UNIX/task3-3/main.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    pid_t process_id = fork();
+    if (process_id < 0) {
+        printf("Failed to create a new process :( \n");
+        exit(-1);
+    } else if (process_id == 0) {
+        printf("==== Process child ====\n");
+        (void) execle("/bin/cat", "/bin/cat", "main.c", 0, envp);
+        printf("Failed to create a new process :( \n");
+        exit(-1);
+    } else {
+        printf("==== Process parent ====\n");
+    }
+    return 0;
+}
