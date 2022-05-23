e499a8cf1bb93de06b39f9eeb29232dc3d04f34a
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-05-23 15:23:29 +0300

    Lab 13 update

--- Семинар 13. Файловые системы/task13-1/main.c
+++ Семинар 13. Файловые системы/task13-1/main.c
@@ -10,30 +10,33 @@ int main() {
     char prev_name[1024] = "a.txt";
     char new_name[1024] = "a0";
     char file_name[1024];
-    if ((fd = open(path_name, O_CREAT | O_EXCL, 0666)) < 0) {
+    if ((fd = open(path_name, O_CREAT | O_EXCL, 0666)) >= 0) {
+        while (1) {
+            char buffer[4];
+            strcpy(file_name, new_name);
+            if (symlink(prev_name, file_name) < 0) {
+                printf("ERROR [task13-1] while creating a link\n");
+                exit(-1);
+            } else {
+                size_t destination_size = sizeof(new_name);
+                strncpy(prev_name, new_name, destination_size);
+                prev_name[destination_size - 1] = '\0';
+                strcpy(new_name, "a");
+                sprintf(buffer, "%d", ++counter);
+                strcat(new_name, buffer);
+                new_name[sizeof(new_name) - 1] = '\0';
+                if (close(fd) < 0) {
+                    exit(-1);
+                }
+            }
+            if ((fd = open(file_name, O_RDONLY, 0666)) < 0) {
+                break;
+            }
+        }
+        printf("Total depth is %d\n", counter - 1);
+        return 0;
+    } else {
         printf("ERROR [task13-1] while creating a file");
         exit(-1);
     }
-    while ((fd = open(file_name, O_RDONLY, 0666)) >= 0) {
-        char buffer[4];
-        strcpy(file_name, new_name);
-        if (symlink(prev_name, file_name) >= 0) {
-            size_t destination_size = sizeof(new_name);
-            strncpy(prev_name, new_name, destination_size);
-            prev_name[destination_size - 1] = '\0';
-            strcpy(new_name, "a");
-            ++counter;
-            sprintf(buffer, "%d", counter);
-            strcat(new_name, buffer);
-            new_name[sizeof(new_name) - 1] = '\0';
-            if (close(fd) < 0) {
-                exit(-1);
-            }
-        } else {
-            printf("ERROR [task13-1] while creating a link\n");
-            exit(-1);
-        }
-    }
-    printf("Total depth is %d\n", counter);
-    return 0;
 }
